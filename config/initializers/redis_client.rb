89b804b8aef6949a74e1ea19f275c91058f0a69e
Zhendong Yin <zhendongyin@brandeis.edu>
2021-11-01 20:49:58 -0400

    fix redis confifg

--- config/initializers/redis_client.rb
+++ config/initializers/redis_client.rb
@@ -1,3 +1,9 @@
-$redis_client = Redis.new(host: 'localhost', port: 6379)
-$login_redis_client = Redis.new(host: 'localhost', port: 6379, db: 1)
-$email_redis_client = Redis.new(host: 'localhost', port: 6379, db: 2)
+if Rails.env.production?
+  $redis_client = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  $login_redis_client = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, db: 1)
+  $email_redis_client = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, db: 2)
+else
+  $redis_client = Redis.new(host: 'localhost', port: 6379)
+  $login_redis_client = Redis.new(host: 'localhost', port: 6379, db: 1)
+  $email_redis_client = Redis.new(host: 'localhost', port: 6379, db: 2)
+end
